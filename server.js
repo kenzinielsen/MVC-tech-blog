1550800e804ddaeec83e8316b6173739b97b9c71
kenzinielsen <kenzinielsen@kenzis-air.lan>
2021-10-28 18:16:08 -0600

    add index.js

--- server.js
+++ server.js
@@ -2,15 +2,17 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const routes = require('./controllers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const hbs = exphbs.create({ helpers });
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: 'Super secret',
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -20,8 +22,6 @@ const sess = {
 };
 
 app.use(session(sess));
-
-const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
